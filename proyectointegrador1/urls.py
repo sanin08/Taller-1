fd1d311888a55ad9a6c73076d82d6720589fce64
sanin08 <jssaninv@eafit.edu.co>
2020-03-04 17:49:20 -0500

    Se agregó la carpeta ultrasonic y se modificaron los archivos models,apps,views, urls

--- proyectointegrador1/urls.py
+++ proyectointegrador1/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+from ultrasonic import views
+
+router = routers.DefaultRouter()
+router.register(r'ultrasonic', views.UltrasonicViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
